39e6748d0be0b6376723b8e4091ef995a6c43f5a
Artur_cs50 <a.mokrzycki@gmail.com>
2021-03-02 17:02:17 +0000

    chapter one done

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# from django.http import HttpResponse # do testowania
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-# Create your views here.
-
+# This is a function based view
+"""
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts}) # tu byl ukryty blad zwiazany z custom managerem "Manager isn’t accessible via <<models>> instances" - {'posts': posts} byl napisany mala litera
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 post in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page in not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
 
-    # return HttpResponse("dupa") # do testowania
-
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts}) # dictionary arguments deliver variables to HTML trmplate
+"""
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day) # czy ma byc podwojne podkreslenie ?
     return render(request, 'blog/post/detail.html', {'post': post})
